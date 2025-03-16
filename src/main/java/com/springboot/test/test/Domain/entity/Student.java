b58edc3abcb6481164f4c8ecd9b8f5c09ff492b2
SethmiDanoshi <111180840+SethmiDanoshi@users.noreply.github.com>
2025-03-16 11:50:56 +0530

    Lombok

--- src/main/java/com/springboot/test/test/Domain/entity/Student.java
+++ src/main/java/com/springboot/test/test/Domain/entity/Student.java
@@ -3,8 +3,13 @@ package com.springboot.test.test.Domain.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.*;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
 public class Student {
     @Id
     private Integer id;
@@ -12,48 +17,5 @@ public class Student {
     private String address;
     private String grade;
 
-
-    public Student(Integer id, String name, String address, String grade) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.grade = grade;
-    }
-
-    public Student() {
-    }
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getGrade() {
-        return grade;
-    }
-
-    public void setGrade(String grade) {
-        this.grade = grade;
-    }
 
 }
